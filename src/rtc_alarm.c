07ab0a62e2b50dee5599f29e6f2dc4735199c045
Kristin Ebuengan <ebuengankris@gmail.com>
2023-07-07 14:25:20 -0700

    added to_bcd function

--- src/rtc_alarm.c
+++ src/rtc_alarm.c
@@ -21,6 +21,19 @@ static uint8_t from_bcd(uint8_t bcd)
 	return (bcd & 0xF) + ((bcd >> 4) * 10);
 }
 
+uint8_t to_bcd(uint8_t value)
+{
+	uint8_t result = 0;
+	uint8_t decade = 0;
+	while(value != 0){
+		uint8_t digit = value % 10;
+		value = value / 10;
+		result |= digit << (4 * decade);
+		decade += 1;
+	}
+	return result;
+}
+
 /**
  * Reads the time in the RTC's alarm registers
  * Since there is no year, we set the year to 0 (i.e., 0 years since 1900)
@@ -58,35 +71,24 @@ void am1815_write_alarm(struct am1815 *rtc, struct timeval *atime)
     gmtime_r(&(atime->tv_sec), &date);
     int hundredths = atime->tv_usec / 10000;
 
-	/*
-    am1815_write_register(rtc, 0x8, (uint8_t)hundredths);
-    am1815_write_register(rtc, 0x9, (uint8_t)date.tm_sec);
-    am1815_write_register(rtc, 0xA, (uint8_t)date.tm_min);
-    am1815_write_register(rtc, 0xB, (uint8_t)date.tm_hour);
-    am1815_write_register(rtc, 0xC, (uint8_t)date.tm_mday);
-    am1815_write_register(rtc, 0xD, (uint8_t)date.tm_mon);
-    am1815_write_register(rtc, 0xE, (uint8_t)date.tm_wday);
-	*/
-
-	am1815_write_register(rtc, 0x8, (uint8_t)10);
-    am1815_write_register(rtc, 0x9, (uint8_t)10);
-    am1815_write_register(rtc, 0xA, (uint8_t)10);
-    am1815_write_register(rtc, 0xB, (uint8_t)10);
-    am1815_write_register(rtc, 0xC, (uint8_t)10);
-    am1815_write_register(rtc, 0xD, (uint8_t)10);
-    am1815_write_register(rtc, 0xE, (uint8_t)5);
+    am1815_write_register(rtc, 0x8, to_bcd((uint8_t)hundredths));
+    am1815_write_register(rtc, 0x9, to_bcd((uint8_t)date.tm_sec));
+    am1815_write_register(rtc, 0xA, to_bcd((uint8_t)date.tm_min));
+    am1815_write_register(rtc, 0xB, to_bcd((uint8_t)date.tm_hour));
+    am1815_write_register(rtc, 0xC, to_bcd((uint8_t)date.tm_mday));
+    am1815_write_register(rtc, 0xD, to_bcd((uint8_t)date.tm_mon));
+    am1815_write_register(rtc, 0xE, to_bcd((uint8_t)date.tm_wday));
 }
 
 static struct uart uart;
 static struct spi spi;
 
 int main(void){
-	spi_init(&spi, 0);
+	spi_init(&spi, 0, 2000000u);
 	spi_enable(&spi);
 
     struct am1815 rtc;
     am1815_init(&rtc, &spi);
-    // initialize_time(&rtc);
 
     uart_init(&uart, UART_INST0);
 
