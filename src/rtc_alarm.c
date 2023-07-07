23938f5ab561d9e9dada2c63d8c8d668bd4430d3
Kristin Ebuengan <ebuengankris@gmail.com>
2023-07-06 17:32:16 -0700

    added write alarm

--- src/rtc_alarm.c
+++ src/rtc_alarm.c
@@ -52,6 +52,21 @@ struct timeval am1815_read_alarm(struct am1815 *rtc)
 	return result;
 }
 
+void am1815_write_alarm(struct am1815 *rtc, struct timeval *atime)
+{
+    struct tm date;
+    gmtime_r(&(atime->tv_sec), &date);
+    int hundredths = atime->tv_usec / 10000;
+
+    am1815_write_register(rtc, 0x8, hundredths);
+    am1815_write_register(rtc, 0x9, date.tm_sec);
+    am1815_write_register(rtc, 0xA, date.tm_min);
+    am1815_write_register(rtc, 0xB, date.tm_hour);
+    am1815_write_register(rtc, 0xC, date.tm_mday);
+    am1815_write_register(rtc, 0xD, date.tm_mon);
+    am1815_write_register(rtc, 0xE, date.tm_wday);
+}
+
 static struct uart uart;
 static struct spi spi;
 
