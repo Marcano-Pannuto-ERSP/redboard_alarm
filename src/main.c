8fc31d198369ae8ba339882f8329246357e37749
Kristin Ebuengan <ebuengankris@gmail.com>
2023-07-31 16:10:27 -0700

    write countdown asimple testing

--- src/main.c
+++ src/main.c
@@ -1,16 +1,35 @@
-#include <am1815.h>
-#include <time.h>
+#include "am_mcu_apollo.h"
+#include "am_bsp.h"
+#include "am_util.h"
+
 #include <sys/time.h>
 #include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
 
-int am1815_read_timer(struct am1815 *rtc)
+#include <uart.h>
+#include <spi.h>
+#include <adc.h>
+#include <am1815.h>
+#include <bmp280.h>
+#include <flash.h>
+#include <pdm.h>
+#include <syscalls.h>
+#include <asimple_littlefs.h>
+
+uint8_t am1815_read_timer(struct am1815 *rtc)
 {
-	uint8_t buffer;
+	uint32_t buffer[1];
 	uint8_t *data = (uint8_t*)buffer;
-	spi_device_cmd_read(rtc->spi, 0x19, buffer, 1);
+	spi_device_cmd_read(rtc->spi, 0x19, data, 1);
 	memcpy(data, buffer, 1);
-	
-    return (int)data;
+
+	// am_util_stdio_printf("Read Timer: %02X\r\n", *data);
+    return data;
+
 	// struct tm date = {
     //     .tm_year = 0,
 	// 	.tm_mon = from_bcd(data[5] & 0x1F) - 1,
@@ -30,29 +49,108 @@ int am1815_read_timer(struct am1815 *rtc)
 	// return result;
 }
 
-// void am1815_write_alarm(struct am1815 *rtc, struct timeval *atime)
-// {
-//     // struct tm date;
-//     // gmtime_r(&(atime->tv_sec), &date);
-//     // int hundredths = atime->tv_usec / 10000;
+double find_timer(double timer)
+{
+    if(timer <= 0.0625){
+        timer = ((int)(timer * 4096))/4096;
+	}
+    else if(timer <= 4){
+        timer = ((int)(timer * 64))/64;
+	}
+    else if(timer <= 256){
+        timer = (int)timer;
+	}
+    else if(timer <= 15360){
+        timer = ((int)(timer/60)) * 60;
+	}
+    else{
+        timer = 15360;
+	}
 
-//     // am1815_write_register(rtc, 0x8, to_bcd((uint8_t)hundredths));
-//     // am1815_write_register(rtc, 0x9, to_bcd((uint8_t)date.tm_sec));
-//     // am1815_write_register(rtc, 0xA, to_bcd((uint8_t)date.tm_min));
-//     // am1815_write_register(rtc, 0xB, to_bcd((uint8_t)date.tm_hour));
-//     // am1815_write_register(rtc, 0xC, to_bcd((uint8_t)date.tm_mday));
-//     // am1815_write_register(rtc, 0xD, to_bcd((uint8_t)date.tm_mon));
-//     // am1815_write_register(rtc, 0xE, to_bcd((uint8_t)date.tm_wday));
-// }
+    if(timer == 0){
+        printf("Timer disabled (set to 0 seconds)");
+	}
+    else{
+        printf("Timer set to: %u", timer);
+	}
+    return timer;
+}
+
+void am1815_write_timer(struct am1815 *rtc, double timer)
+{
+	double finalTimer = find_timer(timer);
+	// TE (enables countdown timer)
+    // Sets the Countdown Timer Frequency and
+    // the Timer Initial Value
+	am_util_stdio_printf("RTC ID: %02X\r\n", am1815_read_register(rtc, 0x28));
+    uint8_t countdowntimer = am1815_read_register(rtc, 0x18);
+    // clear TE first
+	am1815_write_register(rtc, 0x18, countdowntimer & ~0b10000000);
+    uint8_t RPT = countdowntimer & 0b00011100;
+    uint8_t timerResult = 0b10100000 + RPT;
+    uint32_t timerinitial = 0;
+    if(finalTimer <= 0.0625){
+        timerResult += 0b00;
+        timerinitial = ((int)(finalTimer * 4096)) - 1;
+	}
+    else if(finalTimer <= 4){
+        timerResult += 0b01;
+        timerinitial = ((int)(finalTimer * 64)) - 1;
+	}
+    else if(finalTimer <= 256){
+        timerResult += 0b10;
+        timerinitial = ((int)timer) - 1;
+	}
+    else{
+        timerResult += 0b11;
+        timerinitial = ((int)(finalTimer * (1/60))) - 1;
+	}
+
+    if(finalTimer == 0){
+        am_util_stdio_printf("Timer disabled (set to 0 seconds)\r\n");
+	}
+    else{
+        am_util_stdio_printf("Timer set to: %f\r\n", finalTimer);
+	}
+	am1815_write_register(rtc, 0x19, timerinitial);
+	am1815_write_register(rtc, 0x1A, timerinitial);
+	am1815_write_register(rtc, 0x18, timerResult);
+
+	am_util_stdio_printf("18: %02X\r\n", am1815_read_register(rtc, 0x18));
+	am_util_stdio_printf("19: %02X\r\n", am1815_read_register(rtc, 0x19));
+	am_util_stdio_printf("1A: %02X\r\n", am1815_read_register(rtc, 0x1A));
+}
 
 struct am1815 rtc;
 struct spi_bus spi;
 struct spi_device rtc_spi;
+static struct uart uart;
 
 int main(void){
+	// Prepare MCU by init-ing clock, cache, and power level operation
+	am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
+	am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+	am_hal_cachectrl_enable();
+	am_bsp_low_power_init();
+	am_hal_sysctrl_fpu_enable();
+	am_hal_sysctrl_fpu_stacking_enable(true);
+
+	// Init UART, registers with SDK printf
+	syscalls_uart_init(&uart);
+	uart_init(&uart, UART_INST0);
+
+	am_util_stdio_printf("hello!\r\n");
+
 	spi_bus_init(&spi, 0);
 	spi_bus_enable(&spi);
 	spi_bus_init_device(&spi, &rtc_spi, SPI_CS_3, 2000000u);
     am1815_init(&rtc, &rtc_spi);
     am1815_read_timer(&rtc);
+
+	am_util_stdio_printf("Read Register: %02X\r\n", am1815_read_register(&rtc, 0x19));
+	am_util_stdio_printf("RTC ID: %02X\r\n", am1815_read_register(&rtc, 0x28));
+
+	am1815_write_timer(&rtc, 0.5);
+
+	am_util_stdio_printf("done!\r\n");
 }
